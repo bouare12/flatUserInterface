121e0fac7b6e6d7b36412b9f2fe605cc25534f3d
bouare12 <bandiougoubouare@yahoo.fr>
2016-10-21 23:33:48 +0200

    jflat

--- srcipts/flat.js
+++ srcipts/flat.js
@@ -1,11 +1,9 @@
-jQuery(document).ready(function()
-{
-   // On cache la zone de texte
-   jQuery('#toggle').hide();
-   // toggle() lorsque le lien avec l'ID #toggler est cliqué
-   jQuery('a#toggler').click(function()
-  {
-      jQuery('#toggle').toggle(400);
-      return false;
-   });
+jQuery(document).ready(function(){
+
+  $(".soumenu").hide();
+
+  $("a").click(function(){
+    $(this).parent().next().slideToggle();
+
+  });
 });
